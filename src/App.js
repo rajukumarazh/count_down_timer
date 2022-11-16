0b60adfe3d4f78b3d23ee96c09226d40db32d204
rajuk12 <raju.kumar@palinfocom.com>
2022-11-16 16:38:13 +0530

    preparing for test

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Timer from './Timer';
 function App() {
-	const [timer, setTimer] = useState({ hour: 0, min: 0, sec: 60 });
+	const [hoursMinSecs, sethoursMinSecs] = useState({
+		hour: 0,
+		min: 0,
+		sec: 0,
+	});
+	console.log('timer', hoursMinSecs);
 	const [time, setTime] = useState();
 	const [runner, setRunner] = useState({ hour: null, min: null, sec: null });
 	// console.log('timer', timer);
 
-	function convert() {
-		let hour = Number(timer.hour * 3600 * 1000);
-		let min = Number(timer.min * 60 * 1000);
-		let sec = Number(timer.sec * 1000);
-		console.log('hllll', hour + min + sec);
-		let totalTime = hour + min + sec;
-		console.log('total', totalTime);
-		setTime(totalTime);
-		setRunner(timer);
-	}
+	// function convert() {
+	// 	let hour = Number(timer.hour * 3600 * 1000);
+	// 	let min = Number(timer.min * 60 * 1000);
+	// 	let sec = Number(timer.sec * 1000);
+	// 	console.log('hllll', hour + min + sec);
+	// 	let totalTime = hour + min + sec;
+	// 	console.log('total', totalTime);
+	// 	setTime(totalTime);
+	// 	setRunner(timer);
+	// }
 	// console.log('runner', runner);
 
 	console.log('runner');
@@ -26,34 +31,38 @@ function App() {
 				<div className="bg-green-400 flex justify-center border-spacing-2 border-red-700 shadow-lg max-w-lg shadow-slate-500 ">
 					<input
 						type="text"
-						value={runner.hour}
+						// value={runner.hour}
 						placeholder="Hour"
-						onChange={(e) => setTimer({ ...timer, hour: e.target.value })}
+						onChange={(e) => sethoursMinSecs({ hours: e.target.value })}
 						className="rounded-md p-2 border-collapse border-spacing-1 hower-green-500 mt-5 border-red-600 w-12 mb-5 ml-2 mr-2"
 					></input>
 					<input
 						type="text"
-						onChange={(e) => setTimer({ ...timer, min: e.target.value })}
-						value={runner.min}
+						onChange={(e) =>
+							sethoursMinSecs({ ...hoursMinSecs, minutes: e.target.value })
+						}
+						// value={runner.min}
 						placeholder="Min"
 						className="rounded-md  p-2 border-collapse border-spacing-1 hower-green-500 mt-5 border-red-600 w-12 mb-5 ml-2 mr-2"
 					></input>
 					<input
 						type="text"
-						onChange={(e) => setTimer({ ...timer, sec: e.target.value })}
-						value={runner.sec}
+						onChange={(e) =>
+							sethoursMinSecs({ ...hoursMinSecs, seconds: e.target.value })
+						}
+						// value={runner.sec}
 						//  placeholder={time.sec}
 						className="rounded-md  p-2 border-collapse border-spacing-1 hower-green-500 mt-5 border-red-600 w-12 mb-5 ml-2 mr-2"
 					></input>
 				</div>
 			</div>
-			<button
+			{/* <button
 				// onClick={() => runTimer()}
 				className="absolute  bg-red-500 px-2 py-2 top-24 right-1/2 rounded-lg w-16 text-white"
 			>
 				Start
-			</button>
-			<Timer state={timer} />
+			</button> */}
+			<Timer hoursMinSecs={hoursMinSecs} />
 		</div>
 	);
 }
--- src/Timer.js
+++ src/Timer.js
@@ -1,34 +1,50 @@
 import React from 'react';
 import { useState } from 'react';
-function Timer(props) {
-	const { hour = 0, min = 0, sec = 0 } = props.state;
-	console.log('props', hour);
-	const [[hours, mins, secs], setTime] = useState([hour, min, sec]);
-	console.log('secst', secs);
+function Timer({ hoursMinSecs }) {
+	const [start, setStart] = useState(false);
+	const [data, setData] = useState({ h: '', m: '', s: 25 });
+	const { hours = 0, minutes = 0, seconds = data.s } = hoursMinSecs;
+	// console.log('kkkkk', hoursMinSecs);
+	// const [start, setStart] = useState(false);
+	const [[hrs, mins, secs], setTime] = React.useState([
+		hours,
+		minutes,
+		seconds,
+	]);
 
-	const reset = () =>
-		setTime([parseInt(hours), parseInt(mins), parseInt(secs)]);
-	const runTimer = () => {
-		if (hours == 0 && mins == 0 && secs == 0) {
-			reset();
-		} else if (mins == 0 && secs == 0) {
-			setTime([hours - 1, 59, 59]);
-		} else if (secs == 0) {
-			setTime([hours, mins - 1, 59]);
+	const tick = () => {
+		if (hrs === 0 && mins === 0 && secs === 0) reset();
+		else if (mins === 0 && secs === 0) {
+			setTime([hrs - 1, 59, 59]);
+		} else if (secs === 0) {
+			setTime([hrs, mins - 1, 59]);
 		} else {
-			setTime([hours, mins, secs - 1]);
+			setTime([hrs, mins, secs - 1]);
 		}
 	};
+
+	const reset = () =>
+		setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+
 	React.useEffect(() => {
-		const timerId = setInterval(() => runTimer(), 1000);
-		return () => clearInterval(timerId);
+		if (start == true) {
+			const timerId = setInterval(() => tick(), 1000);
+			return () => clearInterval(timerId);
+		}
 	});
+	console.log('tick', secs, mins);
 	return (
 		<div>
 			<h1>
 				Remaining Time is 0{hours}:0{mins}:0{secs}
 			</h1>
-			<button onClick={() => runTimer()} className="bg-red-500">
+			<button
+				onClick={() => {
+					setStart(() => true);
+					console.log('cliekc');
+				}}
+				className="bg-red-500"
+			>
 				start{' '}
 			</button>
 		</div>
